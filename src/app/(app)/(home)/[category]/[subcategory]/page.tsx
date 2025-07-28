35247433f451841c446c7bd91ce2eaa0bf83dcfc
abcphong <158237060+abcphong@users.noreply.github.com>
2025-07-28 09:16:49 +0700

    Product pages

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,30 +1,36 @@
-import { ProductsList , ProductListSkeletion } from "@/modules/products/ui/components/product-list";
+
 import { getQueryClient , trpc} from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { SearchParams } from "nuqs/server";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { loadProductFilters } from "@/modules/products/search-params";
+
 
 interface Props{
     params: Promise<{
         subcategory: string;
     }>
+    searchParams: Promise<SearchParams>;
 };
 
-const Page = async ({ params }: Props) =>{
+const Page = async ({ params, searchParams }: Props) =>{
 
     const { subcategory } = await params;
+    const filters = await loadProductFilters(searchParams);
+
 
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
-        category : subcategory,
+        category: subcategory,
+        ...filters,
     }));
 
 
     return(
         <HydrationBoundary state = {dehydrate(queryClient)}>
-            <Suspense fallback ={<ProductListSkeletion/>}>
-                <ProductsList category = {subcategory}/>
-            </Suspense>
+            <ProductListView category={subcategory}/>
         </HydrationBoundary>
+            
     );
 };
 
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,13 +1,16 @@
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import type { Sort, Where } from "payload";
-import { Category } from "@/payload-types"
+import { Category ,Media} from "@/payload-types"
 
 import z from "zod";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure.input(z.object({
+      cursor:z.number().default(1),
+      limit:z.number().default(DEFAULT_LIMIT),
       category: z.string().nullable().optional(),
       minPrice: z.string().nullable().optional(),
       maxPrice: z.string().nullable().optional(),
@@ -96,9 +99,16 @@ export const productsRouter = createTRPCRouter({
     collection: "products",
     depth: 1, // Tạo "category" & "image"
     where,
-    sort
+    sort,
+    page: input.cursor,
+    limit: input.limit,
     });
-
-        return data;
+        return {
+          ...data,
+          docs: data.docs.map((doc) => ({
+            ...doc,
+            image: doc.image as Media | null,
+          }))
+        }
     }),
 });
