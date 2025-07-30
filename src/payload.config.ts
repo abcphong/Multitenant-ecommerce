334bc9901491a18eac844a5208dba5a0551678a0
abcphong <158237060+abcphong@users.noreply.github.com>
2025-07-31 00:46:15 +0700

    Refresh repository

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -17,6 +17,7 @@ import { Tenants } from './collections/Tenants'
 import { Orders } from './collections/Orders'
 import {  Reviews } from './collections/Reviews'
 import { isSuperAdmin } from './lib/access'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob';
 
 
 const filename = fileURLToPath(import.meta.url)
@@ -54,7 +55,13 @@ export default buildConfig({
         includeDefaultField:false,
       },
       userHasAccessToAllTenants: (user) => isSuperAdmin(user)
-    })
-    // storage-adapter-placeholder
+    }),
+    vercelBlobStorage({
+      enabled:true,
+      collections:{
+        media:true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 })
